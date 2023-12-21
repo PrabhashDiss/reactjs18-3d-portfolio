ec1c4cb0fca8065e624ddb7356c46d056e976e8e
Prabhash Dissanayake <101409417+PrabhashDiss@users.noreply.github.com>
2023-12-21 04:32:30 +0000

    Update App.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,15 +16,15 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915EFF]">Liron</span>
+            Hi, I'm <span className="text-[#915EFF]">Prabhash</span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+          {/* <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop 3D visuals, user <br className="sm:block hidden" />
             interfaces and web applications
-          </p>
+          </p> */}
         </div>
       </div>
-      
+
       <ComputersCanvas />
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
