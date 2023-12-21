efc76754559f08fe341c9753ca0ef330cd3a81d5
Prabhash Dissanayake <101409417+PrabhashDiss@users.noreply.github.com>
2023-12-21 04:32:45 +0000

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,18 +47,21 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I'm a skilled software developer with experience in TypeScript and
+        {/* I'm a skilled software developer with experience in TypeScript and
         JavaScript, and expertise in frameworks like React, Node.js, and
         Three.js. I'm a quick learner and collaborate closely with clients to
         create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        real-world problems. Let's work together to bring your ideas to life! */}
+        I'm going to use this platform to showcase my valuable learning
+        materials. Feel free to contact me if you have specific requests or
+        would like to learn something new.
       </motion.p>
 
-      <div className="mt-20 flex flex-wrap gap-10">
+      {/* <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
-      </div>
+      </div> */}
     </>
   );
 };
